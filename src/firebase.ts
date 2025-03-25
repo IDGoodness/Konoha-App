3408178c39744e6156d82e7ac968521345f6e0b4
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-03-25 15:13:41 +0100

    First step in securing my API keys. Next to vercel

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDRE6Fs8BMZVPqbNQ3Cw7nyDLAh1wKZ6zI",
-    authDomain: "konoha-app-3e063.firebaseapp.com",
-    projectId: "konoha-app-3e063",
-    storageBucket: "konoha-app-3e063.firebasestorage.app",
-    messagingSenderId: "557540801155",
-    appId: "1:557540801155:web:ff0430311351929a36b9b6",
-    measurementId: "G-P71DLVYB02"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
